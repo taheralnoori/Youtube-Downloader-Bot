3eacd9a61789b8fa4689d4074eb3af6a3874764d
taheralnoori <82815443+taheralnoori@users.noreply.github.com>
2022-03-14 21:40:47 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,7 +9,7 @@ async def start(client, message):
         [InlineKeyboardButton(
             "🗼 موسوعة المهندس الكهربائي", url="https://electrical-engineer-cc40b.web.app/")],
         [InlineKeyboardButton(
-            "😴 مطور البوت", url="https://t.me/aryanvikash")]
+            "😴 مطور البوت", url="https://t.me/ta_ja199")]
     ])
     welcomed = f"😇 مرحبا  <b>{message.from_user.first_name}</b>\nانا بوت اقوم بتنزيل اي فيديو على اليوتيوب 🔴\n 😎كل ما عليك هو ارسال رابط  فيديو من اليوتيوب الي\nوبعدها اختار الصيغة ملف (mp3 & mp4) وبعدها الدقة وانتظر 😉\nMade by:@ta_ja199 👨‍💻"
     await message.reply_text(welcomed, reply_markup=joinButton)
